35b485831512d7fd72b362d7c35426daee60a974
Iwan Firmawan <firmawaneiwan@gmail.com>
2024-11-21 21:05:04 +0700

    [#1] Setup leaflet in next.config.mjs & export assets

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,26 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import CopyPlugin from "copy-webpack-plugin";
+
 /** @type {import('next').NextConfig} */
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "node_modules/leaflet/dist/images",
+            to: path.resolve(__dirname, "public", "leaflet", "images"),
+          },
+        ],
+      })
+    );
+    return config;
+  },
   async rewrites() {
     return [
       {
