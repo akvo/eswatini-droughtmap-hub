88f78508907775c2d5b61a6d8387b55a61299bd3
Iwan Firmawan <firmawaneiwan@gmail.com>
2024-11-21 21:06:44 +0700

    [#1] Create TrixEditor & export it from components

---
+++ frontend/src/components/TrixEditor.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Trix from "trix";
+import { ReactTrixRTEInput } from "react-trix-rte";
+
+const TrixEditor = (props) => {
+  const [value, setValue] = useState("");
+  return (
+    <ReactTrixRTEInput
+      className="w-full min-h-20 block"
+      value={value}
+      onChange={(_, v) => setValue(v)}
+      {...props}
+    />
+  );
+};
+
+export default TrixEditor;
