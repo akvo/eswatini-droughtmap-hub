b058fabc02fda0d4569de23342cb9685ad08f432
Iwan Firmawan <firmawaneiwan@gmail.com>
2024-12-04 10:19:18 +0700

    [#1] Create tinymce component

---
+++ frontend/src/components/TinyEditor.js
@@ -0,0 +1,49 @@
+"use client";
+import { Editor } from "@tinymce/tinymce-react";
+import React, { useRef } from "react";
+
+const TinyEditor = ({ value, setValue }) => {
+  const editorRef = useRef(null);
+
+  return (
+    <Editor
+      tinymceScriptSrc={"/assets/libs/tinymce/tinymce.min.js"}
+      onInit={(evt, editor) => (editorRef.current = editor)}
+      value={value}
+      init={{
+        height: 500,
+        menubar: true,
+        plugins: [
+          "advlist",
+          "autolink",
+          "lists",
+          "link",
+          "image",
+          "charmap",
+          "preview",
+          "anchor",
+          "searchreplace",
+          "visualblocks",
+          "code",
+          "fullscreen",
+          "insertdatetime",
+          "media",
+          "table",
+          "code",
+          "help",
+          "wordcount",
+        ],
+        toolbar:
+          "undo redo | blocks | " +
+          "bold italic forecolor | alignleft aligncenter " +
+          "alignright alignjustify | bullist numlist outdent indent | " +
+          "removeformat | help",
+        content_style:
+          "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+      }}
+      onEditorChange={setValue}
+    />
+  );
+};
+
+export default TinyEditor;
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,2 +1,3 @@
 export * from "./Map";
 export { default as TrixEditor } from "./TrixEditor";
+export { default as TinyEditor } from "./TinyEditor";
