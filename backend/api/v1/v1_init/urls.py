f052837318c28f371cc254b7b640b8ab0739e59f
Iwan Firmawan <firmawaneiwan@gmail.com>
2024-12-05 10:38:26 +0700

    [#1] Remove initial jobs endpoints

--- backend/api/v1/v1_init/urls.py
+++ backend/api/v1/v1_init/urls.py
@@ -1,6 +1,5 @@
 from django.urls import re_path
 from . import views
-from .views import job_check, create_job
 
 urlpatterns = [
     re_path(
@@ -8,6 +7,4 @@ urlpatterns = [
         views.health_check,
         name="health_check",
     ),
-    re_path(r"^(?P<version>(v1))/jobs/create", create_job),
-    re_path(r"^(?P<version>(v1))/jobs/check", job_check),
 ]
