f931aa698194f3673bb9ec3caa8285ce08df0571
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-03 13:39:25 +0700

    feat: add new endpoint /api/v1/config.js

--- backend/api/v1/v1_init/urls.py
+++ backend/api/v1/v1_init/urls.py
@@ -1,5 +1,6 @@
 from django.urls import re_path
 from . import views
+from .views import get_config_file
 
 urlpatterns = [
     re_path(
@@ -7,4 +8,5 @@ urlpatterns = [
         views.health_check,
         name="health_check",
     ),
+    re_path(r"^(?P<version>(v1))/config.js", get_config_file),
 ]
--- backend/api/v1/v1_init/views.py
+++ backend/api/v1/v1_init/views.py
@@ -1,10 +1,27 @@
+from pathlib import Path
+
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema
+from django.core.management import call_command
+from django.http import HttpResponse
+from jsmin import jsmin
 
 
 @extend_schema(description="Use to check System health", tags=["Dev"])
 @api_view(["GET"])
 def health_check(request, version):
     return Response({"message": "OK"}, status=status.HTTP_200_OK)
+
+
+@extend_schema(description="Get required configuration", tags=["Dev"])
+@api_view(["GET"])
+def get_config_file(request, version):
+    if not Path("source/config/config.min.js").exists():
+        call_command("generate_config")
+    data = jsmin(open("source/config/config.min.js", "r").read())
+    response = HttpResponse(
+        data, content_type="application/x-javascript; charset=utf-8"
+    )
+    return response
