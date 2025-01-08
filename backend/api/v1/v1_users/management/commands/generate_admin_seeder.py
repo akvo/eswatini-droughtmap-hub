7bee63e3dcd09f289933b9b284069388534b9499
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-08 18:19:20 +0700

    [#6] Add new command: generate_admin_seeder

---
+++ backend/api/v1/v1_users/management/commands/generate_admin_seeder.py
@@ -0,0 +1,26 @@
+from django.core.management import BaseCommand
+from api.v1.v1_users.models import SystemUser
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-t", "--test", nargs="?", const=False, default=False, type=bool
+        )
+
+    def handle(self, *args, **options):
+        total_admins = SystemUser.objects.filter(
+            is_superuser=True
+        ).count()
+        index = total_admins + 1
+        email = f"admin{index}@mail.com"
+        SystemUser.objects.create_admin(
+            email=email,
+            password="Changeme123",
+            name=f"Admin {index}",
+        )
+        test = options.get("test")
+        if not test:
+            self.stdout.write(
+                self.style.SUCCESS(f"{email} created successfully")
+            )
