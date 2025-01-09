8c3701929873d528bc9dc31cc775ddc966b0fda6
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-09 22:03:52 +0700

    [#6] Add new test case: resend verification less than 1hr

--- backend/api/v1/v1_users/tests/tests_users_resend_verification_endpoint.py
+++ backend/api/v1/v1_users/tests/tests_users_resend_verification_endpoint.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.management import call_command
 from django.test.utils import override_settings
+from django.utils import timezone
+from datetime import timedelta
 from api.v1.v1_users.tests.mixins import ProfileTestHelperMixin
 from api.v1.v1_users.models import SystemUser
 from api.v1.v1_users.constants import UserRoleTypes
@@ -71,4 +73,30 @@ class ResendVerificationTestCase(TestCase, ProfileTestHelperMixin):
         self.assertEqual(
             res,
             {"message": "Email address not found."}
+        )
+
+    def test_resend_verification_less_than_one_hour(self):
+        code_expiry = timezone.now() + timedelta(hours=1)
+        self.user.email_verification_expiry = code_expiry
+        self.user.save()
+
+        self.assertIsNotNone(self.user.email)
+        req = self.client.post(
+            "/api/v1/email/resend-verify",
+            {
+                "email": self.user.email,
+            },
+            content_type="application/json",
+            HTTP_AUTHORIZATION=f"Bearer {self.token}"
+        )
+        self.assertEqual(req.status_code, 400)
+        res = req.json()
+        self.assertEqual(
+            res,
+            {
+                "message": (
+                    "Verification email already sent. "
+                    "Please wait before requesting a new one."
+                )
+            }
         )
