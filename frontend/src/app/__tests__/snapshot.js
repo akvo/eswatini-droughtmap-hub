52d813f51226c5764f45acf1aa9953250e0e7d2a
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-09 14:16:29 +0700

    [#6] Remove snapshot test

--- frontend/src/app/__tests__/snapshot.js
+++
@@ -1,9 +0,0 @@
-import { render, waitFor } from "@testing-library/react";
-import Home from "../page";
-
-it("renders homepage unchanged", async () => {
-  await waitFor(() => {
-    const { container } = render(<Home />);
-    expect(container).toMatchSnapshot();
-  });
-});
