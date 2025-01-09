915594059660566e92b129638cb9eba716f1b117
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-09 13:08:20 +0700

    [#6] Update tailwind config to add primary color, bg & container

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,6 +10,20 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#3e5eb9",
+      },
+      backgroundImage: {
+        "image-login": "url('/images/bg-image-login.png')",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem", // 640px
+        md: "3rem", // 768px
+        lg: "3rem", // 1024px
+        xl: "4rem", // 1280px
       },
     },
   },
