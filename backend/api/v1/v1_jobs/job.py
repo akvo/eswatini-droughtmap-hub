cf27253cfe090491eb215c78be14d51ba2bc8a88
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-09 22:53:01 +0700

    [#6] Create notification email jobs

--- backend/api/v1/v1_jobs/job.py
+++ backend/api/v1/v1_jobs/job.py
@@ -1,7 +1,10 @@
 from time import sleep
 from django.utils import timezone
+from django.conf import settings
 from api.v1.v1_jobs.models import Jobs
 from api.v1.v1_jobs.constants import JobStatus
+from api.v1.v1_users.models import SystemUser
+from utils.email_helper import send_email, EmailTypes
 
 
 def demo_q_func(name: str):
@@ -10,6 +13,41 @@ def demo_q_func(name: str):
 
 
 def demo_q_response_func(task):
+    job = Jobs.objects.get(task_id=task.id)
+    job.attempt = job.attempt + 1
+    if task.success:
+        job.status = JobStatus.done
+        job.available = timezone.now()
+    else:
+        job.status = JobStatus.failed
+    job.result = task.result
+    job.save()
+
+
+def notify_verification_email(email: str, code: str):
+    if not settings.TEST_ENV:
+        send_email(
+            type=EmailTypes.verification_email,
+            context={
+                "send_to": [email],
+                "verification_code": code,
+            },
+        )
+
+
+def notify_forgot_password(user: SystemUser):
+    if not settings.TEST_ENV:
+        send_email(
+            type=EmailTypes.forgot_password,
+            context={
+                "send_to": [user.email],
+                "name": user.name,
+                "reset_password_code": user.reset_password_code,
+            },
+        )
+
+
+def email_notification_results(task):
     job = Jobs.objects.get(task_id=task.id)
     job.attempt = job.attempt + 1
     if task.success:
