67d7baa18ee29d083eff820be52e87eb0a9ed660
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-09 22:08:21 +0700

    [#6] Create new lib: storage

--- frontend/src/lib/index.js
+++ frontend/src/lib/index.js
@@ -1,4 +1,5 @@
 export * from "./api";
+export * from "./storage";
 import * as auth from "./auth";
 
 export { auth };
---
+++ frontend/src/lib/storage.js
@@ -0,0 +1,34 @@
+"use client";
+
+const set = (name, data) => {
+  window.localStorage.setItem(
+    window.btoa(name),
+    window.btoa(JSON.stringify(data))
+  );
+  return data;
+};
+
+const get = (name) => {
+  name = window.btoa(name);
+  const data = window.localStorage.getItem(name);
+  if (!data) {
+    return false;
+  }
+  let response = false;
+  try {
+    response = JSON.parse(window.atob(data));
+  } catch (err) {
+    window.localStorage.clear();
+  }
+  return response;
+};
+
+const clear = () => {
+  return window.localStorage.clear();
+};
+
+export const storage = {
+  set: set,
+  get: get,
+  clear: clear,
+};
