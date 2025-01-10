c096d996aec284adc97242aa414fa02d309642f9
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-10 16:40:48 +0700

    [#4] Check ownership

--- backend/test.sh
+++ backend/test.sh
@@ -18,21 +18,11 @@ coverage combine --rcfile=./.coveragerc
 coverage report -m --rcfile=./.coveragerc
 
 if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]]; then
-    # Navigate to script's directory
-    SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-    echo "SCRIPT_DIR: ${SCRIPT_DIR}"
-    # cd "$SCRIPT_DIR"
-
-    # Set Git root and safe directory
     export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
-    GIT_ROOT=$(git rev-parse --show-toplevel)
-    git config --global --add safe.directory "$GIT_ROOT"
+    git config --global --add safe.directory /app
 
-    # Debugging
-    echo "Git root: $GIT_ROOT"
-    echo "Safe directories: $(git config --global --get-all safe.directory)"
+    chown -R $(id -u):$(id -g) /app
 
-    # Run Coveralls
     coveralls --verbose
 fi
 
