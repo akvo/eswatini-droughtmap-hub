146c25ee4f9824c4b36b48ae82d172c040d8ca36
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-10 16:23:28 +0700

    [#4] Modify git config in backend/test.sh

--- backend/test.sh
+++ backend/test.sh
@@ -18,8 +18,20 @@ coverage combine --rcfile=./.coveragerc
 coverage report -m --rcfile=./.coveragerc
 
 if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]]; then
+    # Navigate to script's directory
+    SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+    cd "$SCRIPT_DIR"
+
+    # Set Git root and safe directory
     export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
-    git config --global --add safe.directory /app
+    GIT_ROOT=$(git rev-parse --show-toplevel)
+    git config --global --add safe.directory "$GIT_ROOT"
+    export GIT_ROOT
+
+    echo "Git root: $GIT_ROOT"
+    echo "Safe directories: $(git config --global --get-all safe.directory)"
+
+    # Run Coveralls
     coveralls --verbose
 fi
 
