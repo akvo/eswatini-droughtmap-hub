300e4ddc7af065fc98c2d019d56c67d10fc4f775
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-10 16:55:34 +0700

    [#4] revert test.sh

--- backend/test.sh
+++ backend/test.sh
@@ -18,18 +18,9 @@ coverage combine --rcfile=./.coveragerc
 coverage report -m --rcfile=./.coveragerc
 
 if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]]; then
-     # Allow Git to traverse filesystems
     export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+    git config --global --add safe.directory /app/backend
 
-    # Mark the backend directory's root as safe for Git
-    GIT_ROOT=$(git rev-parse --show-toplevel)
-    git config --global --add safe.directory "$GIT_ROOT"
-
-    # Debugging to ensure Git is configured correctly
-    echo "Git root: $GIT_ROOT"
-    echo "Safe directories: $(git config --global --get-all safe.directory)"
-
-    # Run Coveralls
     coveralls --verbose
 fi
 
