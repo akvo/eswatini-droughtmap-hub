d5469555a25cdb52d99c4e0bd8874adb02843943
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-11 23:18:54 +0700

    [#8] Add new constants: TWG & ActionEnum

--- backend/api/v1/v1_users/constants.py
+++ backend/api/v1/v1_users/constants.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 class UserRoleTypes:
     admin = 1
     reviewer = 2
@@ -6,3 +9,32 @@ class UserRoleTypes:
         admin: "Admin",
         reviewer: "Reviewer",
     }
+
+
+# Technical Working Group list
+# https://www.figma.com/design/gtNfp5n7NawbYW5u8cPrpT?node-id=3-2#1050735747
+class TechnicalWorkingGroup:
+    ndma = 1
+    moag = 2
+    met = 3
+    dwa = 4
+    uneswa = 5
+
+    FieldStr = {
+        ndma: "NDMA (National Disaster Management Agency)",
+        moag: "MoAg (Ministry of Agriculture)",
+        met: "MET (Meteorological Office)",
+        dwa: "DWA (Department of Water Affairs)",
+        uneswa: "UNESWA (University of Eswatini)",
+    }
+
+
+class ActionEnum(Enum):
+    CREATE = 'create'
+    READ = 'read'
+    UPDATE = 'update'
+    DELETE = 'delete'
+
+    @classmethod
+    def choices(cls):
+        return [(tag.value, tag.name.capitalize()) for tag in cls]
