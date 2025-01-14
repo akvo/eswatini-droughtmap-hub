d9a2c9df41e89d55e1f4e340a61414dffccf1836
ifirmawan <firmawaneiwan@gmail.com>
2025-01-14 16:52:00 +0700

    [#13] Implements reviews list in table view

--- frontend/src/app/reviews/page.js
+++ frontend/src/app/reviews/page.js
@@ -1,41 +1,91 @@
 "use client";
 
+import { useCallback, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Table, Tag, Typography } from "antd";
 import { Can } from "@/components";
+import { api } from "@/lib";
+import { PAGE_SIZE } from "@/static/config";
 
-class Entity {
-  constructor(attrs) {
-    Object.assign(this, attrs);
-  }
-}
-
-class Review extends Entity {}
+const { Title } = Typography;
 
 const ReviewsPage = () => {
-  const someoneReview = new Review({ owner: "false" });
-  const myReview = new Review({ owner: "true" });
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [totalData, setTotalData] = useState(0);
+  const router = useRouter();
+
+  const columns = [
+    {
+      title: "DEADLINE",
+      dataIndex: "due_date",
+      key: "due_date",
+      defaultSortOrder: "ascend",
+      sorter: (a, b) => new Date(a.due_date) - new Date(b.due_date),
+    },
+    {
+      title: "MONTH",
+      dataIndex: "year_month",
+      key: "year_month",
+    },
+    {
+      title: "TINKHUNDLA",
+      dataIndex: "progress_review",
+      key: "progress_review",
+    },
+    {
+      title: "STATUS",
+      dataIndex: "is_completed",
+      key: "is_completed",
+      render: (_, { is_completed }) => (
+        <Tag color={is_completed ? "success" : "#6b7280"}>
+          {is_completed ? "Completed" : "Pending"}
+        </Tag>
+      ),
+    },
+  ];
+
+  const fetchData = useCallback(async () => {
+    const { data, total } = await api("GET", "/reviewer/reviews");
+    setTotalData(total);
+    const _reviews = data.map((d) => ({ key: d?.id, ...d }));
+    setReviews(_reviews);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="w-full h-auto space-y-4">
-      <p>You are signed in as a Reviewer</p>
-
-      <ul>
-        <Can I="create" a="Review">
-          <li>Yes, you can create a review</li>
-        </Can>
-        <Can I="create" a="Publication">
-          <li>
-            <del>No, you can't</del>
-          </li>
-        </Can>
-        <Can I="read" a="Publication">
-          <li>Yes, you can read Publication</li>
-        </Can>
-        <Can I="update" a={someoneReview}>
-          <li>No, you can't</li>
-        </Can>
-        <Can I="update" a={myReview}>
-          <li>Yes, you can update your Review</li>
-        </Can>
-      </ul>
+      <Title level={2}>My reviews</Title>
+      <Can I="read" a="Review">
+        <Table
+          columns={columns}
+          dataSource={reviews}
+          loading={loading}
+          onRow={(record) => {
+            return {
+              onClick: () => {
+                router.push(`/reviews/${record.id}`);
+              },
+            };
+          }}
+          rowClassName={"cursor-pointer"}
+          pagination={
+            totalData < PAGE_SIZE
+              ? false
+              : {
+                  pageSize: PAGE_SIZE,
+                  total: totalData,
+                  responsive: true,
+                  align: "center",
+                  position: ["bottomCenter"],
+                }
+          }
+        />
+      </Can>
     </div>
   );
 };
