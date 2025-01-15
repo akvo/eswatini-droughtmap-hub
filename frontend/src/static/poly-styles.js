3395d2b1fe725cd48d1fd391131930a5a65f05ac
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-15 15:52:21 +0700

    [#13] Create new static: Poly Styles for leaflet.pattern

---
+++ frontend/src/static/poly-styles.js
@@ -0,0 +1,27 @@
+const styleOptions = {
+  color: "green",
+  fillOpacity: 1,
+  opacity: 0.9,
+  weight: 4,
+};
+
+const patternOptions = {
+  height: 12,
+  width: 12,
+  spaceColor: "#fff",
+  weight: 2,
+  spaceWeight: 8,
+  opacity: 0.75,
+  spaceOpacity: 0.45,
+};
+
+const dotShapeOptions = {
+  x: 8,
+  y: 8,
+  radius: 4,
+  fill: true,
+  stroke: false,
+  fillOpacity: 0.75,
+};
+
+export { styleOptions, patternOptions, dotShapeOptions };
