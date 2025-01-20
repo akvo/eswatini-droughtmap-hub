ca1629839d55bceda97320d3cbf7a86bfa16453b
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-20 22:55:06 +0700

    [#17] Refactoring CDIMap to fix hooks issue (useMap)
    [#17] Rename GeoPattern to CDIGeoJSON

--- frontend/src/components/Map/CDIMap.js
+++ frontend/src/components/Map/CDIMap.js
@@ -2,60 +2,103 @@
 
 import L from "leaflet";
 import "leaflet.pattern";
-import { DEFAULT_CENTER } from "@/static/config";
+import { DEFAULT_CENTER, DROUGHT_CATEGORY } from "@/static/config";
 import Map from "./Map";
-import { useMap } from "react-leaflet";
+import { useMap, GeoJSON } from "react-leaflet";
 import {
   dotShapeOptions,
   patternOptions,
   styleOptions,
 } from "@/static/poly-styles";
-import { useAppContext } from "@/context/AppContextProvider";
+import { useAppContext, useAppDispatch } from "@/context/AppContextProvider";
+import { Flex, Spin } from "antd";
 
-const CDIMap = () => {
+const CDIGeoJSON = ({ geoData, onEachFeature, style }) => {
+  const map = useMap();
+  const { refreshMap } = useAppContext();
+
+  if (refreshMap) {
+    return (
+      <Flex align="center" justify="center" className="w-full h-full" vertical>
+        <Spin tip="Updating..." />
+      </Flex>
+    );
+  }
+
+  return (
+    <GeoJSON
+      key="geodata"
+      data={geoData}
+      weight={1}
+      onEachFeature={(feature, layer) => onEachFeature(feature, layer, map)}
+      style={style}
+    />
+  );
+};
+
+const CDIMap = ({ data = [] }) => {
   const appContext = useAppContext();
   const geoData = appContext?.geoData || null;
+  const { selectedAdms = [], isBulkAction = false, activeAdm } = appContext;
+  const appDispatch = useAppDispatch();
 
-  const onEachFeature = (feature, layer) => {
+  const geoJSONStyle = (feature) => {
+    const isSelected = selectedAdms.includes(
+      feature?.properties?.administration_id
+    );
+    const isHighighted =
+      isBulkAction ||
+      isSelected ||
+      feature?.properties?.administration_id === activeAdm?.administration_id;
+    const findAdm = data?.find(
+      (d) => d?.administration_id === feature?.properties?.administration_id
+    );
+    return {
+      opacity: isHighighted ? 1 : styleOptions?.opacity,
+      weight: isHighighted ? 4 : findAdm?.reviewed ? 3 : styleOptions?.weight,
+      color: isHighighted || !findAdm?.reviewed ? styleOptions?.color : "green",
+    };
+  };
+
+  const onEachFeature = (feature, layer, currentMap) => {
     if (feature.geometry.type === "Polygon") {
-      const shapeColors = [
-        "#730000",
-        "#E60000",
-        "#FFAA00",
-        "#FCD37F",
-        "#FFFF00",
-        "#FFFFFF",
-      ];
-      const randomColor =
-        shapeColors[Math.floor(Math.random() * shapeColors.length)];
+      const findAdm = data?.find(
+        (d) => d?.administration_id === feature?.properties?.administration_id
+      );
+      const category =
+        findAdm?.category === null
+          ? findAdm?.initial_category
+          : findAdm?.category;
+      const fillColor =
+        DROUGHT_CATEGORY?.[category]?.color || DROUGHT_CATEGORY[0].color;
       const shape = new L.PatternCircle({
         ...dotShapeOptions,
-        fillColor: randomColor,
+        fillColor,
       });
       const pattern = new L.Pattern(patternOptions);
       pattern.addShape(shape);
-      pattern.addTo(useMap());
+      pattern.addTo(currentMap);
       layer.setStyle({
         ...styleOptions,
         fillPattern: pattern,
-        color: "#485D92",
+        weight: findAdm?.reviewed ? 4 : styleOptions?.weight,
+        color: findAdm?.reviewed ? "green" : styleOptions?.color,
+      });
+      layer.on({
+        click: () => {
+          appDispatch({
+            type: "SET_ACTIVE_ADM",
+            payload: { ...findAdm, name: feature?.properties?.name },
+          });
+        },
       });
     }
   };
 
   return (
     <Map center={DEFAULT_CENTER} height={160} zoom={9} minZoom={9}>
-      {({ GeoJSON }) => (
-        <>
-          {geoData && (
-            <GeoJSON
-              key="geodata"
-              data={geoData}
-              weight={1}
-              onEachFeature={onEachFeature}
-            />
-          )}
-        </>
+      {() => (
+        <CDIGeoJSON {...{ geoData, onEachFeature }} style={geoJSONStyle} />
       )}
     </Map>
   );
