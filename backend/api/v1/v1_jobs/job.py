c4a0a95872ad0568c3d9bd79f3917965faba24de
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-28 17:20:21 +0700

    [#23] Create 3 new jobs to start publication process
    1. Download geonode dataset
    2. Generate initial cdi values
    3. Send review request mail

--- backend/api/v1/v1_jobs/job.py
+++ backend/api/v1/v1_jobs/job.py
@@ -1,10 +1,28 @@
+import logging
+import os
+import time
+import rasterio
+import geopandas as gpd
+import requests
+from rasterstats import zonal_stats
 from time import sleep
 from django.utils import timezone
 from django.conf import settings
+from django_q.tasks import async_task
 from api.v1.v1_jobs.models import Jobs
-from api.v1.v1_jobs.constants import JobStatus
+from api.v1.v1_jobs.constants import JobStatus, JobTypes
 from api.v1.v1_users.models import SystemUser
+from api.v1.v1_publication.models import Publication
+from api.v1.v1_publication.serializers import (
+    ReviewSerializer,
+    PublicationSerializer,
+)
+from api.v1.v1_publication.utils import get_category
 from utils.email_helper import send_email, EmailTypes
+
+# Set up logging
+logger = logging.getLogger(__name__)
+tmp_dir = "./tmp"
 
 
 def demo_q_func(name: str):
@@ -87,6 +105,10 @@ def notify_review_request(
                 "body": body,
             }
         )
+    return {
+        "email": email,
+        "review_id": review_id,
+    }
 
 
 def email_notification_results(task):
@@ -95,6 +117,174 @@ def email_notification_results(task):
     if task.success:
         job.status = JobStatus.done
         job.available = timezone.now()
+    else:
+        job.status = JobStatus.failed
+    job.result = task.result
+    job.save()
+
+
+def download_geonode_dataset(
+    download_url: str,
+    filename: str,
+):
+    # Create /tmp directory if it doesn't exist
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+
+    # Download and store it in /tmp directory
+    input_file = os.path.join(tmp_dir, filename)
+    response = requests.get(download_url, stream=True)
+    if response.status_code == 200:
+        with open(input_file, "wb") as f:
+            # Write the response in chunks to handle large files
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:  # Filter out keep-alive new chunks
+                    f.write(chunk)
+        return input_file
+    else:
+        logger.error(
+            f"Failed to download the file from {download_url}."
+            f"Status code: {response.status_code}"
+        )
+        return False
+
+
+def download_geonode_dataset_results(task):
+    job = Jobs.objects.get(task_id=task.id)
+    job.attempt = job.attempt + 1
+    job_info = job.info
+    publication_id = job_info["publication_id"]
+    filename = job_info["filename"]
+    subject = job_info["subject"]
+    message = job_info["message"]
+
+    input_file = os.path.join(tmp_dir, filename)
+
+    if task.success and os.path.exists(input_file):
+        job.status = JobStatus.done
+        job.available = timezone.now()
+
+        # Create a job
+        job = Jobs.objects.create(
+            type=JobTypes.initial_cdi_values,
+            status=JobStatus.on_progress,
+            info={
+                "id": publication_id,
+                "subject": subject,
+                "message": message,
+            },
+        )
+        hook = "api.v1.v1_jobs.job.generate_initial_cdi_values_results"
+        task_id = async_task(
+            "api.v1.v1_jobs.job.generate_initial_cdi_values",
+            publication_id,
+            input_file,
+            hook=hook,
+        )
+        # Update the job with the task ID
+        job.task_id = task_id
+        job.save()
+    else:
+        job.status = JobStatus.failed
+    job.result = task.result
+    job.save()
+
+
+def generate_initial_cdi_values(
+    publication_id: int,
+    input_file: str,
+):
+    publication = Publication.objects.filter(
+        pk=publication_id
+    ).first()
+    # Read the topojson file to load all Administrations
+    topojson_file = "./source/eswatini.topojson"
+    gdf = gpd.read_file(topojson_file)
+
+    gdf.crs = "epsg:4326"
+
+    # Ensure the CRS of the GeoDataFrame and the raster are the same
+    with rasterio.open(input_file) as src:
+        gdf = gdf.to_crs(src.crs)
+
+    # Perform zonal statistics
+    zs = zonal_stats(
+        gdf,
+        input_file,
+        stats=["median"],  # Specify the statistics you need
+        geojson_out=True  # Output as GeoJSON features for easier handling
+    )
+    # Map the zonal statistics to a new column in the GeoDataFrame
+    gdf["raster_values"] = [feat["properties"] for feat in zs]
+
+    # Convert raster_values to separate columns for easier analysis
+    gdf["value"] = gdf["raster_values"].apply(
+        lambda x: x.get("median", None)
+    )
+    # Add a new column 'initial_category' using the get_category function
+    gdf["initial_category"] = gdf["value"].apply(get_category)
+
+    # Remove the original 'raster_values' column if not needed
+    gdf = gdf.drop(columns=["raster_values"])
+
+    # Now you have columns like 'value' in your GeoDataFrame
+    result = gdf[["administration_id", "value", "initial_category"]]
+
+    publication.initial_values = result.to_dict(orient="records")
+    publication.save()
+    return PublicationSerializer(publication).data
+
+
+def generate_initial_cdi_values_results(task):
+    job = Jobs.objects.get(task_id=task.id)
+    job.attempt = job.attempt + 1
+    job_info = job.info
+    subject = job_info["subject"]
+    message = job_info["message"]
+
+    publication = Publication.objects.filter(
+        pk=job_info["id"]
+    ).first()
+
+    if task.success and publication and len(publication.initial_values):
+        job.status = JobStatus.done
+        job.available = timezone.now()
+
+        for review in publication.reviews.all():
+            # Create a job
+            job = Jobs.objects.create(
+                type=JobTypes.review_request,
+                status=JobStatus.on_progress,
+                result=ReviewSerializer(review).data,
+            )
+            # Replace placeholders in the message
+            body = message \
+                .replace(
+                    "{{reviewer_name}}",
+                    review.user.name
+                ) \
+                .replace(
+                    "{{year_month}}",
+                    publication.year_month.strftime("%Y-%m")
+                ) \
+                .replace(
+                    "{{due_date}}",
+                    publication.due_date.strftime("%Y-%m-%d")
+                )
+
+            # Dispatch the send email job for each reviewer
+            task_id = async_task(
+                "api.v1.v1_jobs.job.notify_review_request",
+                review.user.email,
+                review.id,
+                subject,
+                body,
+                hook="api.v1.v1_jobs.job.email_notification_results",
+            )
+            # Update the job with the task ID
+            job.task_id = task_id
+            job.save()
+
     else:
         job.status = JobStatus.failed
     job.result = task.result
