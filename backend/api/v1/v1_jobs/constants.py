608e99b37a907390b236e1ecc353a6bdf61c615b
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-01-28 09:33:00 +0700

    [#23] Add new JobTypes: review_request & initial_cdi_values

--- backend/api/v1/v1_jobs/constants.py
+++ backend/api/v1/v1_jobs/constants.py
@@ -3,12 +3,16 @@ class JobTypes:
     verification_email = 2
     forgot_password = 3
     review_completed = 4
+    review_request = 5
+    initial_cdi_values = 6
 
     FieldStr = {
         test: "test",
         verification_email: "verification_email",
         forgot_password: "forgot_password",
         review_completed: "review_completed",
+        review_request: "review_request",
+        initial_cdi_values: "initial_cdi_values",
     }
 
 
