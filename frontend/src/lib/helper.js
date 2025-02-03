d8beb6584df4d3524413acbd62e15327029a3e9a
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-02-03 08:17:02 +0700

    [#31] Remove helper in library

--- frontend/src/lib/helper.js
+++
@@ -1,4 +0,0 @@
-import { DROUGHT_CATEGORY } from "@/static/config";
-
-export const findCategory = (category) =>
-  DROUGHT_CATEGORY.find((c) => c.value === category);
--- frontend/src/lib/index.js
+++ frontend/src/lib/index.js
@@ -1,7 +1,6 @@
 export * from "./api";
 export * from "./storage";
 export * from "./ability";
-export * from "./helper";
 import * as auth from "./auth";
 
 export { auth };
