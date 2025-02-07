0442b21f853358d2129cad60657e2e543eb441c3
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-02-07 16:11:11 +0700

    [#38] Implements DashboardLayout in publications, reviews & profile page

--- frontend/src/app/publications/layout.js
+++ frontend/src/app/publications/layout.js
@@ -1,30 +1,13 @@
-import Link from "next/link";
-import { Button } from "antd";
 import { UserContextProvider } from "@/context";
 import { auth } from "@/lib";
+import { DashboardLayout } from "@/components";
 
 const PublicationLayout = async ({ children }) => {
   const session = await auth.getSession();
   const abilities = session?.abilities || [];
   return (
     <UserContextProvider abilities={abilities}>
-      <div className="w-full h-screen">
-        <div className="w-full py-2 bg-primary sticky top-0 z-50">
-          <div className="w-full container flex flex-row justify-between items-center">
-            <div>
-              <Link href={"/publications"} className="text-white">
-                Dashboard
-              </Link>
-            </div>
-            <div>
-              <Link href={"/profile"}>
-                <Button>Profile</Button>
-              </Link>
-            </div>
-          </div>
-        </div>
-        <main className="w-full container">{children}</main>
-      </div>
+      <DashboardLayout user={session}>{children}</DashboardLayout>
     </UserContextProvider>
   );
 };
