8b6faba20091b46fafd43a84ed68835c23de1015
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-02-17 15:27:49 +0700

    [#46] Update settings page to add CDI Automation section

--- frontend/src/app/settings/page.js
+++ frontend/src/app/settings/page.js
@@ -3,15 +3,86 @@
 import { SubmitButton } from "@/components";
 import { MinusCircleIcon, PlusCircleIcon } from "@/components/Icons";
 import { api } from "@/lib";
-import { Button, Flex, Form, Input, Space, Typography } from "antd";
+import { Button, Divider, Flex, Form, Input, Space, Typography } from "antd";
 import { useCallback, useEffect, useState } from "react";
 
 const { Title, Text } = Typography;
 const { useForm } = Form;
 
+const EmailListForm = ({ label, name }) => (
+  <Form.List
+    name={name}
+    rules={[
+      {
+        validator: async (_, names) => {
+          if (!names || names.length < 1) {
+            return Promise.reject(new Error("At least 1 emails"));
+          }
+        },
+      },
+    ]}
+  >
+    {(fields, { add, remove }, { errors }) => (
+      <div className="w-full">
+        <Flex align="center" justify="space-between">
+          <div>
+            <Text strong>{label}</Text>
+          </div>
+          <div>
+            <Button
+              type="dashed"
+              onClick={() => add()}
+              icon={<PlusCircleIcon />}
+            >
+              Add new
+            </Button>
+          </div>
+        </Flex>
+        {fields.map(({ key, ...field }) => (
+          <Flex key={key} vertical>
+            <Space className="w-full mb-4">
+              <Form.Item
+                {...field}
+                validateTrigger={["onChange", "onBlur"]}
+                rules={[
+                  {
+                    required: true,
+                    whitespace: true,
+                    type: "email",
+                    message: "This is not a valid email",
+                  },
+                ]}
+                className="w-full"
+              >
+                <Input placeholder="Email" type="email" className="w-full" />
+              </Form.Item>
+              {fields.length > 0 ? (
+                <a
+                  role="button"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    remove(field.name);
+                  }}
+                >
+                  <MinusCircleIcon />
+                </a>
+              ) : null}
+            </Space>
+          </Flex>
+        ))}
+        <Form.Item>
+          <Form.ErrorList errors={errors} />
+        </Form.Item>
+      </div>
+    )}
+  </Form.List>
+);
+
 const SettingsPage = () => {
   const [settings, setSettings] = useState({
-    ts_emails: [null],
+    on_success_emails: [null],
+    on_failure_emails: [null],
+    on_exceeded_emails: [null],
   });
   const [loading, setLoading] = useState(false);
   const [form] = useForm();
@@ -19,8 +90,9 @@ const SettingsPage = () => {
   const onFinish = async (payload) => {
     setLoading(true);
     try {
-      const apiData = await api("PUT", "/settings", payload);
-      setSettings(apiData);
+      console.log("payload", payload);
+      // const apiData = await api("PUT", "/settings", payload);
+      // setSettings(apiData);
       setLoading(false);
     } catch (err) {
       console.error(err);
@@ -28,105 +100,51 @@ const SettingsPage = () => {
     }
   };
 
-  const fetchData = useCallback(async () => {
-    try {
-      const apiData = await api("GET", "/settings");
+  // const fetchData = useCallback(async () => {
+  //   try {
+  //     const apiData = await api("GET", "/settings");
 
-      setSettings(apiData);
-      if (apiData?.ts_emails) {
-        form.setFieldValue("ts_emails", apiData.ts_emails);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }, [form]);
+  //     setSettings(apiData);
+  //     if (apiData?.on_failure_emails) {
+  //       form.setFieldValue("on_failure_emails", apiData.on_failure_emails);
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }, [form]);
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+  // useEffect(() => {
+  //   fetchData();
+  // }, [fetchData]);
 
   return (
-    <div className="w-1/2 h-auto space-y-4 pt-6">
-      <Flex align="center" justify="space-between">
-        <div className="w-2/3">
-          <Title level={2}>Settings</Title>
-        </div>
-      </Flex>
-      <Form form={form} initialValues={settings} onFinish={onFinish}>
-        <Flex gap={16}>
-          <div>
-            <Text strong>Technical Support Contacts:</Text>
-          </div>
-          <div>
-            <Form.List
-              name="ts_emails"
-              rules={[
-                {
-                  validator: async (_, names) => {
-                    if (!names || names.length < 1) {
-                      return Promise.reject(new Error("At least 1 emails"));
-                    }
-                  },
-                },
-              ]}
-            >
-              {(fields, { add, remove }, { errors }) => (
-                <>
-                  {fields.map(({ key, ...field }) => (
-                    <Flex key={key} vertical>
-                      <Space className="w-full mb-4">
-                        <Form.Item
-                          {...field}
-                          validateTrigger={["onChange", "onBlur"]}
-                          rules={[
-                            {
-                              required: true,
-                              whitespace: true,
-                              type: "email",
-                              message: "This is not a valid email",
-                            },
-                          ]}
-                        >
-                          <Input
-                            placeholder="Email"
-                            type="email"
-                            className="w-full"
-                          />
-                        </Form.Item>
-                        {fields.length > 0 ? (
-                          <a
-                            role="button"
-                            onClick={(e) => {
-                              e.preventDefault();
-                              remove(field.name);
-                            }}
-                          >
-                            <MinusCircleIcon />
-                          </a>
-                        ) : null}
-                      </Space>
-                    </Flex>
-                  ))}
-                  <Form.Item>
-                    <Button
-                      type="dashed"
-                      onClick={() => add()}
-                      icon={<PlusCircleIcon />}
-                    >
-                      Add new
-                    </Button>
+    <div className="w-full h-auto space-y-4 pt-6">
+      <Title level={2}>Settings</Title>
+      <div className="w-full h-auto flex flex-col lg:flex-row align-start justify-between gap-6">
+        <div className="w-full lg:w-4/12 space-y-2">
+          <Title level={3}>Email Notifications</Title>
+          <Form form={form} initialValues={settings} onFinish={onFinish}>
+            <EmailListForm
+              label="On Success (DH Admins)"
+              name="on_success_emails"
+            />
+            <EmailListForm
+              label="On Failure (Technical Support)"
+              name="on_failure_emails"
+            />
+            <EmailListForm label="On Job Exceeded" name="on_exceeded_emails" />
 
-                    <Form.ErrorList errors={errors} />
-                  </Form.Item>
-                </>
-              )}
-            </Form.List>
-          </div>
-        </Flex>
-        <SubmitButton size="large" form={form} loading={loading}>
-          Save
-        </SubmitButton>
-      </Form>
+            <div className="w-full text-right">
+              <SubmitButton size="large" form={form} loading={loading}>
+                Save
+              </SubmitButton>
+            </div>
+          </Form>
+        </div>
+        <div className="w-full lg:w-8/12 border-l border-l-grey-100 px-6">
+          <Title level={3}>CDI Automation Jobs</Title>
+        </div>
+      </div>
     </div>
   );
 };
