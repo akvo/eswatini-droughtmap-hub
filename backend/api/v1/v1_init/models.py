5e6eeb63f11cf4a211814f44f1dfcaf85a383936
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-02-17 11:06:53 +0700

    [#46] Remove all unnecessary resources in v1_init due rundeck compatibility

--- backend/api/v1/v1_init/models.py
+++ backend/api/v1/v1_init/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
-
-class Settings(models.Model):
-    ts_emails = models.JSONField(
-        null=True,
-        blank=True
-    )
-    secret_key = models.CharField(
-        max_length=255,
-        unique=True,
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        db_table = "settings"
+# Create your models here.
--- backend/api/v1/v1_init/urls.py
+++ backend/api/v1/v1_init/urls.py
@@ -1,10 +1,6 @@
 from django.urls import re_path
 from api.v1.v1_init.views import (
     health_check,
-    SecretKeyView,
-    TSContactView,
-    AdminContactView,
-    SettingsView,
 )
 
 urlpatterns = [
@@ -12,25 +8,5 @@ urlpatterns = [
         r"^(?P<version>(v1))/health/check",
         health_check,
         name="health_check",
-    ),
-    re_path(
-        r"^(?P<version>(v1))/settings/secret-key",
-        SecretKeyView.as_view(),
-        name="secret_key",
-    ),
-    re_path(
-        r"^(?P<version>(v1))/settings",
-        SettingsView.as_view(),
-        name="settings",
-    ),
-    re_path(
-        r"^(?P<version>(v1))/contacts/ts",
-        TSContactView.as_view(),
-        name="contacts_ts",
-    ),
-    re_path(
-        r"^(?P<version>(v1))/contacts/dha",
-        AdminContactView.as_view(),
-        name="contacts_admin",
     ),
 ]
