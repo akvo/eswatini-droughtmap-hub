5bc8382dea6c06f1d6526018a76f64733b25704a
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-02-17 11:07:58 +0700

    [#46] Create Settings model & generate migrations in v1_rundeck

---
+++ backend/api/v1/v1_rundeck/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Settings(models.Model):
+    project_name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    job_id = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    on_success_emails = models.JSONField(
+        null=True,
+        blank=True
+    )
+    on_failure_emails = models.JSONField(
+        null=True,
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = "settings"
