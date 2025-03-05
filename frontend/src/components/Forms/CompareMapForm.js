cac5ce14af6a147364679e2f06b78c77914ad748
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-03-05 09:18:36 +0700

    [#49] Replace SelectDate with new component in CompareMapForm

--- frontend/src/components/Forms/CompareMapForm.js
+++ frontend/src/components/Forms/CompareMapForm.js
@@ -1,33 +1,10 @@
 "use client";
 
-import { Flex, Select, Space } from "antd";
+import { Flex } from "antd";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import dayjs from "dayjs";
-import advancedFormat from "dayjs/plugin/advancedFormat";
-import customParseFormat from "dayjs/plugin/customParseFormat";
+import SelectDate from "../SelectDate";
 
-dayjs.extend(advancedFormat);
-dayjs.extend(customParseFormat);
-
-const SelectDate = ({ label, options, onChange, value }) => (
-  <div className="text-sm font-bold bg-primary text-white pl-6 pr-1 py-1 rounded-md">
-    <Space size="large">
-      <span>{label}</span>
-      <Select
-        className="w-48"
-        placeholder="Select Publication Date"
-        options={options.map((o) => ({
-          ...o,
-          label: dayjs(o.label).format("MMMM YYYY"),
-        }))}
-        onChange={onChange}
-        value={value}
-        allowClear
-      />
-    </Space>
-  </div>
-);
 
 const CompareMapForm = ({ dates = [], searchParams }) => {
   const [leftOptions, setLeftOptions] = useState(dates);
