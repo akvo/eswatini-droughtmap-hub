15ebd97685e9f6554ac5835c1d275a31093b0c9b
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-03-20 11:44:21 +0700

    [#71] Add feedback serializer and feedback API endpoint for job feedback submission

--- backend/api/v1/v1_jobs/views.py
+++ backend/api/v1/v1_jobs/views.py
@@ -12,7 +12,9 @@ from drf_spectacular.types import OpenApiTypes
 from django_q.tasks import async_task
 from .models import Jobs
 from .constants import JobTypes, JobStatus
-from .serializers import JobSerializer, CreateJobSerializer
+from .serializers import JobSerializer, CreateJobSerializer, FeedbackSerializer
+from utils.default_serializers import DefaultResponseSerializer
+from utils.custom_serializer_fields import validate_serializers_message
 
 
 @extend_schema(
@@ -73,6 +75,42 @@ def create_job(request, version):
     return Response(
         {
             "job_id": job.id
+        },
+        status=status.HTTP_200_OK
+    )
+
+
+@extend_schema(
+    description="Feedback API",
+    tags=["Jobs"],
+    request=FeedbackSerializer,
+    responses=DefaultResponseSerializer,
+)
+@api_view(["POST"])
+def feedback(request, version):
+    serializer = FeedbackSerializer(data=request.data)
+    if not serializer.is_valid():
+        return Response(
+            {"message": validate_serializers_message(serializer.errors)},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+    email = serializer.validated_data.get("email")
+    feedback = serializer.validated_data.get("feedback")
+    task_id = async_task(
+        "api.v1.v1_jobs.job.notify_feedback_received",
+        email,
+        feedback,
+        hook="api.v1.v1_jobs.job.email_notification_results",
+    )
+    Jobs.objects.create(
+        type=JobTypes.send_feedback,
+        status=JobStatus.on_progress,
+        result=feedback,
+        task_id=task_id,
+    )
+    return Response(
+        {
+            "message": "Feedback received successfully"
         },
         status=status.HTTP_200_OK
     )
