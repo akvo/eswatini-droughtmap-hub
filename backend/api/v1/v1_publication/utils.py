ffc96341d33f5cf0c7f025aef1f19bab31267878
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-05-08 10:24:02 +0700

    [#23] Fix category thresholds in get_category function for accurate drought classification

--- backend/api/v1/v1_publication/utils.py
+++ backend/api/v1/v1_publication/utils.py
@@ -5,14 +5,14 @@ def get_category(value: float):
     value = round(value, 3)
     if (value < 0):
         return DroughtCategory.none
-    if (value >= 0 and value <= 2):
+    if (value >= 0 and value <= 0.02):
         return DroughtCategory.d4
-    if (value > 2 and value <= 5):
+    if (value > 0.02 and value <= 0.05):
         return DroughtCategory.d3
-    if (value > 5 and value <= 10):
+    if (value > 0.05 and value <= 0.1):
         return DroughtCategory.d2
-    if (value > 10 and value <= 20):
+    if (value > 0.1 and value <= 0.2):
         return DroughtCategory.d1
-    if (value > 20 and value <= 30):
+    if (value > 0.2 and value <= 0.3):
         return DroughtCategory.d0
     return DroughtCategory.normal
