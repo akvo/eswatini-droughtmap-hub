aef04f7500c1ed4754193b7c317db2d5967a6853
Iwan Firmawan <firmawaneiwan@gmail.com>
2025-06-12 13:42:42 +0700

    [#15] Update PublicationsPage to use dynamic data source for publications table

--- frontend/src/app/publications/page.js
+++ frontend/src/app/publications/page.js
@@ -188,22 +188,7 @@ const PublicationsPage = () => {
       <Can I="read" a="Publication">
         <Table
           columns={columns}
-          dataSource={[
-            {
-              pk: 1,
-              title: "step_0303_cdi_pct_rank_eswatini_200002",
-              detail_url: "https://geonode.ndma.org.sz/catalogue/#/dataset/1",
-              embed_url:
-                "https://geonode.ndma.org.sz/datasets/geonode:step_0303_cdi_pct_rank_eswatini_200002/embed",
-              thumbnail_url: null,
-              download_url:
-                "https://geonode.ndma.org.sz/datasets/geonode:step_0303_cdi_pct_rank_eswatini_200002/dataset_download",
-              created: "2025-06-10T13:09:59.069019Z",
-              year_month: "2025-06-10T13:09:59.037035Z",
-              publication_id: null,
-              status: null,
-            },
-          ]}
+          dataSource={publications}
           loading={loading}
           rowClassName={"cursor-pointer"}
           pagination={
