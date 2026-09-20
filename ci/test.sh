be24b67e526b6e4a71ff39bdc83343523f906076
Iwan Firmawan <firmawaneiwan@gmail.com>
2026-09-21 06:47:24 +0700

    chore: Disable dbdocs update in backend test workflow

--- ci/test.sh
+++ ci/test.sh
@@ -60,7 +60,7 @@ fi
 if [[ ${BACKEND_CHANGES} == 1 ]]; then
     echo "================== * BACKEND TEST * =================="
     backend_test
-    update_dbdocs
+    # update_dbdocs
 else
     echo "No Changes detected for backend -- SKIP TEST"
 fi
